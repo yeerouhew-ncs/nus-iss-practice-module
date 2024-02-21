36bd4bb9f44e82987a85150ffca08a813b27e960
yeerou.hew <yeerou.hew@ncs.com.sg>
2024-02-21 22:00:06 +0800

    feat: add routes and styling

---
+++ tbs-frontend/src/features/event/containers/Event.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Header from "../../../common/header/Header";
+
+const Event = () => {
+  return (
+    <div>
+      <Header />
+      <div>Event</div>
+    </div>
+  );
+};
+
+export default Event;
---
+++ tbs-frontend/src/features/event/event.api.ts
@@ -0,0 +1,3 @@
+export const createEventApi = async () => {
+  return;
+};
