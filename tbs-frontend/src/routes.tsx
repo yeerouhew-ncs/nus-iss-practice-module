fd9d1c86b494e2ad1528c01232433ffad156eb4a
yeerou.hew <yeerou.hew@ncs.com.sg>
2024-02-29 22:05:51 +0800

    feat: add seating plan svg for testing

--- tbs-frontend/src/routes.tsx
+++ tbs-frontend/src/routes.tsx
@@ -2,13 +2,15 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Event from "./features/event/containers/Event";
 import Login from "./features/authentication/containers/Login";
+import SeatingPlan from "./features/seating-plan/containers/SeatingPlan";
 
 const AppRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<Login />}></Route>
       <Route path="/login" element={<Login />}></Route>
-      <Route path="event" element={<Event />}></Route>
+      <Route path="/event" element={<Event />}></Route>
+      <Route path="/seating-plan" element={<SeatingPlan />}></Route>
     </Routes>
   );
 };
