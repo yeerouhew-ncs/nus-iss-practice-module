c19ef17667efb1b0caabf41443fb052f44646b1f
wanqi.lim <wanqi.lim@ncs.com.sg>
2024-04-10 17:26:24 +0800

    add paypal button linked to PaymentStart.tsx

--- tbs-frontend/src/features/payment/PaymentStart.tsx
+++ tbs-frontend/src/features/payment/PaymentStart.tsx
@@ -1,5 +1,7 @@
 import { useNavigate } from "react-router-dom";
-
+import {client} from "./payment.api";
+import React from 'react';
+import { PayPalButton } from "react-paypal-button-v2";
 
 const PaymentStart: React.FC = () => {
     const navigate = useNavigate();
@@ -8,13 +10,43 @@ const PaymentStart: React.FC = () => {
         navigate("/user/payment/success");
     }
 
+    const onSuccess = (payment:any) => {
+        // Congratulation, it came here means everything's fine!
+        console.log("The payment was succeeded!", payment);
+        // this.props.clearCart();
+        // this.props.history.push('/');
+        // You can bind the "payment" object's value to your state or props or whatever here, please see below for sample returned data
+    }
+    const onCancel = (data:any) => {
+        // User pressed "cancel" or close Paypal's popup!
+        console.log('The payment was cancelled!', data);
+        // You can bind the "data" object's value to your state or props or whatever here, please see below for sample returned data
+    }
+    const onError = (err:any) => {
+        // The main Paypal's script cannot be loaded or somethings block the loading of that script!
+        console.log("Error!", err);
+        // Because the Paypal's main script is loaded asynchronously from "https://www.paypalobjects.com/api/checkout.js"
+        // => sometimes it may take about 0.5 second for everything to get set, or for the button to appear
+    }
+    let currency = 'SGD';
+    let total = '10.00';
+
     return (
         <div>
             <h2>Payment Options</h2>
             <div className={`d-grid gap-2 col-6 mx-auto pt-5`}>
-                <button className={`btn btn-outline-light`} onClick={redirectToPaypal}>
-                    <img src={require('../../images/paypal.png')} className={`img-fluid`} />
-                </button>
+                {/*<button className={`btn btn-outline-light`} onClick={redirectToPaypal}>*/}
+                {/*    <img src={require('../../images/paypal.png')} className={`img-fluid`} />*/}
+                {/*</button>*/}
+                <PayPalButton
+                    amount={total}
+                    currency={currency}
+                    options={client}
+                    shippingPreference="NO_SHIPPING"
+                    onError={onError}
+                    onSuccess={onSuccess}
+                    // onCancel={onCancel}
+                />
             </div>
         </div>
     );
---
+++ tbs-frontend/src/features/payment/payment.api.ts
@@ -0,0 +1,9 @@
+const PAYPAL_CLIENT = 'AVL3t_B9WIEb-CdLDHYK1sW8nXEtp7GM8Qk9m29QTvb-OYyb2dxdo_PapsHFT5KI08OyWiWDNtuL9tI0';
+const PAYPAL_SECRET = 'EF1n5ItUXHKwB8qI7HRGzLtN9M5U9omdMKYTRm0BSvUkRgCr5LLyQvThBN4OJMl3a09yGFU8oXVf8Jpb';
+const PAYPAL_ORDER_API = 'https://api-m.paypal.com/v2/checkout/orders/';
+
+export const client = {
+    clientId: PAYPAL_CLIENT,
+    currency:"SGD",
+
+};
